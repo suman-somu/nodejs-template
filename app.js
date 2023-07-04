6e9573daff58f686c007e683801b1c5b81f4d538
Suman Sahoo <ssamsomu@gmail.com>
2023-07-04 16:48:04 +0530

    added required modules and created a server

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const port = 8080;
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
